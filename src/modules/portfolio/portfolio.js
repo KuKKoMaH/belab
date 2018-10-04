2a3cd94ce36b2cb2cf2cca143487203d33fa18cd
KuKKoMaH <kukkoman@gmail.com>
2018-10-05 00:33:30 +0300

    Initial commit

---
+++ src/modules/portfolio/portfolio.js
@@ -0,0 +1,7 @@
+$('.portfolio__items').addClass('owl-carousel').owlCarousel({
+  stagePadding: 0,
+  autoWidth:    true,
+  dots:         false,
+  nav:          true,
+  navText:      ['', ''],
+});
