bf29e71ea957d37604ef88ccc4df725d4145338a
KuKKoMaH <kukkoman@gmail.com>
2018-10-05 00:37:29 +0300

    init

--- src/app.js
+++ src/app.js
@@ -4,15 +4,10 @@ import SmoothScroll from 'smooth-scroll';
 import 'magnific-popup/dist/jquery.magnific-popup.js';
 import 'jquery.maskedinput/src/jquery.maskedinput';
 
-import './js/$.debounce';
 import './modules/header/header';
 import './modules/portfolio/portfolio';
 
-
-import './modules/slide/slide';
 import './modules/form/form';
-import './modules/catalog/catalog';
-import './modules/delivery/delivery';
 import './modules/faq/faq';
 
 $('input[type="tel"]').mask("+7 (999) 999-99-99");
